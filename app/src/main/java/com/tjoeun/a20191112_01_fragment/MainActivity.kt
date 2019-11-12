7d30b41eddc49912a1ca4b3289ae400964e56d52
kiback2da <kiback2da@steconsulting.co.kr>
2019-11-12 19:49:26 +0900

    첫번째 프래그먼트 생성

--- app/src/main/java/com/tjoeun/a20191112_01_fragment/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191112_01_fragment/MainActivity.kt
@@ -2,6 +2,7 @@ package com.tjoeun.a20191112_01_fragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tjoeun.a20191112_01_fragment.fragments.FirstFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -18,7 +19,7 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
-        mainFirstTxt.text  = "첫 테스트"
+        mainFirstTxt.text  = "첫번째 테스트"
         mainSecondTxt.text = "두번째 테스트"
     }
 
