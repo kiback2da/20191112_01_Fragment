19a4db767c64b86b623a384b8e9cbfa8f853c34c
kiback2da <kiback2da@steconsulting.co.kr>
2019-11-12 19:43:27 +0900

    오늘의 첫 예제를 fragment를 안쓰고 작성

--- app/src/main/java/com/tjoeun/a20191112_01_fragment/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191112_01_fragment/MainActivity.kt
@@ -2,12 +2,15 @@ package com.tjoeun.a20191112_01_fragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
     }
 
     override fun setupEvents() {
@@ -15,7 +18,8 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
-
+        mainFirstTxt.text  = "첫 테스트"
+        mainSecondTxt.text = "두번째 테스트"
     }
 
 }
